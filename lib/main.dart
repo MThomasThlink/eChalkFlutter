53d743c11bb236e62343c6287a9b6658f798482b
Rafael Silva e Souza <rafael.silva@gmail.com>
2023-11-27 11:02:24 -0300

    Add base logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
 
 import 'echalk.dart';
 
 void main() {
+  _setupLogger();
+
   runApp(
     const EChalk(),
+  );
+}
+
+void _setupLogger() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(
+    (record) {
+      debugPrint('${record.level.name} - ${record.time}: ${record.message}');
+    },
   );
 }
